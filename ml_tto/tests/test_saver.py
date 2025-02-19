64fd49210cfddd2cb1b8639309d1d8fff6bec3cc
Ryan Roussel <rroussel@slac.stanford.edu>
2025-02-19 10:32:28 -0600

    Create test_saver.py

---
+++ ml_tto/tests/test_saver.py
@@ -0,0 +1,17 @@
+from ml_tto.saver import H5Saver
+import numpy as np
+
+class TestSaver:
+    def test_nans(self):
+        saver = H5Saver()
+        data = {
+            "a": np.nan,
+            "b": np.inf,
+            "c": -np.inf,
+            "d": [np.nan, np.inf, -np.inf],
+            "e": [np.nan, np.inf, -np.inf, 1.0],
+            "f": [np.nan, np.inf, -np.inf, "a"],
+            "g": {"a": np.nan, "b": np.inf, "c": -np.inf},
+            "h": "np.Nan",
+        }
+        saver.save_to_h5(data, "test.h5")
